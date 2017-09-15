31f0143c20416583b03dc865a8f1d197dbdbafac
Alcereo <ak-91@mail.ru>
2017-09-15 16:50:02 +0300

    A lot of

--- eureka-cli.go
+++ eureka-cli.go
@@ -2,28 +2,18 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	"log"
-	"encoding/json"
-	"io/ioutil"
 	"os"
 	"github.com/urfave/cli"
+	"github.com/alcereo/eureka-cli/eureka-interface"
+	"text/template"
 )
-
-type Application struct {
-	Name string `json:"name"`
-}
-
-type ApplicationMessage struct {
-	Applications []Application `json:"application"`
-}
-
-type ApplicationsResponse struct {
-	Message ApplicationMessage `json:"applications"`
-}
 
 const infoUrlAppNameRequiredCode = 19
 const infoUrlIdRequiredCode = 18
+
+var instancesListTemplate = "" +
+					"{{- printf \"%-20.20s\" \"APP NAME\" }}{{- printf \"%-10.10s\" \"STATUS\"  }}{{- printf \"%-18.18s\" \"ID\"  }}{{- printf \"%-18.18s\" \"IP ADDRESS\"  }}{{- printf \"%-18.18s\" \"PORT\"  }} \n" +
+	"{{range .Items}}{{- printf \"%-20.20s\"  .AppName    }}{{- printf \"%-10.10s\" .Status     }}{{- printf \"%-18.18s\"  .Id    }}{{- printf \"%-18.18s\"  .Ip            }}{{- printf \"%d\"  .Port.Number   }} \n{{end}}"
 
 func main() {
 
@@ -37,17 +27,17 @@ func main() {
 
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
-			Name: "u, host",
-			Value: "localhost",
-			Usage: "IP host adrees of eureka server",
-			EnvVar: "EUREKA_SERVER_HOST",
+			Name:        "u, host",
+			Value:       "localhost",
+			Usage:       "IP host adrees of eureka server",
+			EnvVar:      "EUREKA_SERVER_HOST",
 			Destination: &eurekaHost,
 		},
 		cli.IntFlag{
-			Name: "p, port",
-			Value: 8761,
-			Usage: "Port of eureka server",
-			EnvVar: "EUREKA_SERVER_PORT",
+			Name:        "p, port",
+			Value:       8761,
+			Usage:       "Port of eureka server",
+			EnvVar:      "EUREKA_SERVER_PORT",
 			Destination: &eurekaPort,
 		},
 	}
@@ -56,35 +46,68 @@ func main() {
 		{
 			Name: "info",
 			Description: "Query info about instances \n\n" +
-					"Sample:\n    eureka info -a $APP_NAME -i $INSTANCE_ID",
+				"Sample:\n    eureka info -a $APP_NAME -i $INSTANCE_ID",
 			Usage: "Query info about instances",
 			Flags: []cli.Flag{
 				cli.StringFlag{
-					Name: "a, app-name",
-					Usage: "Application name registered in Eureka server",
+					Name:   "a, app-name",
+					Usage:  "Application name registered in Eureka server",
 					EnvVar: "INFO_SPRING_APPLICATION_NAME",
 				},
 				cli.StringFlag{
-					Name: "i, id",
-					Usage: "Instance ID registered in Eureka server",
+					Name:   "i, id",
+					Usage:  "Instance ID registered in Eureka server",
 					EnvVar: "INFO_EUREKA_INSTANCE_INSTANCE_ID",
 				},
 			},
-			Action:func(context *cli.Context) {
+			Action: func(context *cli.Context) {
 
-				//TODO: EurekaService.GetInstances()
-				fmt.Println("Some fuck! App: ", context.String("app-name"))
+				appName := context.String("app-name")
+				instanceId := context.String("id")
+
+				client := discovery.Client{
+					EurekaHost: eurekaHost,
+					EurekaPort: eurekaPort,
+				}
+
+				var instances []discovery.Instance
+
+				switch {
+				case appName == "" && instanceId == "":
+					{
+						instances = client.GetInstances()
+					}
+				case appName == "":
+					{
+						instances = client.GetInstanceById(instanceId)
+					}
+				case instanceId == "":
+					{
+						instances = client.GetInstancesByApp(appName)
+					}
+				default:
+					{
+						instances = client.GetInstanceByAppAndId(appName, instanceId)
+					}
+				}
+
+				t,_ := template.New("instances").Parse(instancesListTemplate)
+
+				t.Execute(os.Stdout,
+					struct {
+						Items []discovery.Instance
+						}{Items:instances})
 
 			},
 
 			// GET URL
 			Subcommands: cli.Commands{
 				cli.Command{
-					Name: "url",
+					Name:        "url",
 					Description: "Get url of concrete instance",
-					Usage: "Get url of concrete instance",
-					ArgsUsage: "$APP_NAME $INSTANCECE_ID",
-					Action:func(context *cli.Context) error {
+					Usage:       "Get url of concrete instance",
+					ArgsUsage:   "$APP_NAME $INSTANCECE_ID",
+					Action: func(context *cli.Context) error {
 
 						if context.Args().Get(0) == "" {
 							cli.ShowCommandHelpAndExit(context, "url", infoUrlAppNameRequiredCode)
@@ -102,44 +125,9 @@ func main() {
 					},
 				},
 			},
-
 		},
 	}
 
-
 	app.Run(os.Args)
-
-	fmt.Println("Host: ",eurekaHost)
-	fmt.Println("Port: ",eurekaPort)
-
-	return
-	// test
-
-	client := &http.Client{}
-
-	request, err := http.NewRequest("GET", "http://localhost:8761/eureka/apps", nil)
-
-	request.Header.Add("Content-Type", "application/json")
-	request.Header.Add("Accept", "application/json")
-
-	response, err := client.Do(request)
-
-	if err != nil {
-		log.Fatal(err)
-	}else {
-
-		bytes, _ := ioutil.ReadAll(response.Body)
-		response.Body.Close()
-
-		var appResp ApplicationsResponse
-
-		json.Unmarshal(bytes, &appResp)
-
-		for _, els := range appResp.Message.Applications{
-
-			fmt.Println(els.Name)
-		}
-
-	}
 
 }
---
+++ eureka-interface/discovery-client.go
@@ -0,0 +1,212 @@
+package discovery
+
+import (
+	"net/http"
+	"log"
+	"io/ioutil"
+	"encoding/json"
+	"fmt"
+)
+
+type Instance struct {
+	Id string `json:"instanceId"`
+	AppName string `json:"app"`
+	Ip string `json:"ipAddr"`
+	Port struct{
+		Number int `json:"$"`
+	} `json:"port"`
+	Status string `json:"status"`
+}
+
+type Application struct {
+	Name string `json:"name"`
+	Instances []Instance `json:"instance"`
+}
+
+type SingleInstanceApplication struct {
+	Instance Instance `json:"instance"`
+}
+
+type ApplicationMessage struct {
+	Applications []Application `json:"application"`
+}
+
+type SingleApplicationMessage struct {
+	Application Application `json:"application"`
+}
+
+type ApplicationsResponse struct {
+	Message ApplicationMessage `json:"applications"`
+}
+
+
+type Client struct {
+	EurekaHost string
+	EurekaPort int
+}
+
+func (client Client) GetInstanceByAppAndId(appName string, instanceId string) []Instance {
+
+	httpClient := &http.Client{}
+
+	url := fmt.Sprintf(
+		"http://%s:%d/eureka/apps/%s/%s",
+		client.EurekaHost,
+		client.EurekaPort,
+		appName,
+		instanceId,
+	)
+
+	request, err := http.NewRequest(
+		"GET",
+		url,
+		nil)
+
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("Accept", "application/json")
+
+	response, err := httpClient.Do(request)
+
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+
+	bytes, _ := ioutil.ReadAll(response.Body)
+	response.Body.Close()
+
+	var appResp SingleInstanceApplication
+
+	json.Unmarshal(bytes, &appResp)
+
+	instanceArr := []Instance{}
+
+	instanceArr = append(instanceArr, appResp.Instance)
+
+	return instanceArr
+}
+
+func (client Client) GetInstanceById(instanceId string) []Instance {
+	httpClient := &http.Client{}
+
+	url := fmt.Sprintf(
+		"http://%s:%d/eureka/instances/%s",
+		client.EurekaHost,
+		client.EurekaPort,
+		instanceId,
+	)
+
+	request, err := http.NewRequest(
+		"GET",
+		url,
+		nil)
+
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("Accept", "application/json")
+
+	response, err := httpClient.Do(request)
+
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+
+	bytes, _ := ioutil.ReadAll(response.Body)
+	response.Body.Close()
+
+	var appResp SingleInstanceApplication
+
+	json.Unmarshal(bytes, &appResp)
+
+	instanceArr := []Instance{}
+
+	instanceArr = append(instanceArr, appResp.Instance)
+
+	return instanceArr
+}
+
+func (client Client) GetInstancesByApp(appName string) []Instance {
+
+	httpClient := &http.Client{}
+
+	url := fmt.Sprintf(
+		"http://%s:%d/eureka/apps/%s",
+		client.EurekaHost,
+		client.EurekaPort,
+		appName,
+	)
+
+	request, err := http.NewRequest(
+		"GET",
+		url,
+		nil)
+
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("Accept", "application/json")
+
+	response, err := httpClient.Do(request)
+
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+
+	bytes, _ := ioutil.ReadAll(response.Body)
+	response.Body.Close()
+
+	var appResp SingleApplicationMessage
+
+	json.Unmarshal(bytes, &appResp)
+
+	instanceArr := []Instance{}
+
+	for _, instance := range appResp.Application.Instances {
+		instanceArr = append(instanceArr, instance)
+	}
+
+	return instanceArr
+
+}
+
+func (client Client) GetInstances() []Instance {
+
+	httpClient := &http.Client{}
+
+	url := fmt.Sprintf(
+		"http://%s:%d/eureka/apps",
+		client.EurekaHost,
+		client.EurekaPort,
+	)
+
+	request, err := http.NewRequest(
+		"GET",
+		url,
+		nil)
+
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("Accept", "application/json")
+
+	response, err := httpClient.Do(request)
+
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+
+	bytes, _ := ioutil.ReadAll(response.Body)
+	response.Body.Close()
+
+	var appResp ApplicationsResponse
+
+	json.Unmarshal(bytes, &appResp)
+
+	instanceArr := []Instance{}
+
+	for _, application := range appResp.Message.Applications {
+		for _, instance := range application.Instances {
+			instanceArr = append(instanceArr, instance)
+		}
+	}
+
+	return instanceArr
+}
